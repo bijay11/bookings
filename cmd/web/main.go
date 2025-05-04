0698d79220d5156ee6ee0c5a895e9d2cadd4195f
Bijay Tamang <tmg.bijay11@gmail.com>
2025-05-04 09:32:24 -0400

    handle errors

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/bijay11/bookings/internal/config"
 	"github.com/bijay11/bookings/internal/handlers"
+	"github.com/bijay11/bookings/internal/helpers"
 	"github.com/bijay11/bookings/internal/models"
 	"github.com/bijay11/bookings/internal/render"
 )
@@ -18,6 +20,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 
@@ -42,6 +46,13 @@ func run() error {
 
 	// Change this value to true in production
 	app.InProduction = false
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -62,7 +73,8 @@ func run() error {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
+
 	return nil
 }
---
+++ internal/helpers/helpers.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+
+	"github.com/bijay11/bookings/internal/config"
+)
+
+var app *config.AppConfig
+
+func NewHelpers(a *config.AppConfig) {
+	app = a
+}
+
+func ClientError(w http.ResponseWriter, status int) {
+	app.ErrorLog.Println("Client Error:", status)
+	http.Error(w, http.StatusText(status), status)
+}
+
+func ServerError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.ErrorLog.Println(trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
