a087fd8c09ef99abf9e36f13fe90893de87dc797
Bijay Tamang <tmg.bijay11@gmail.com>
2025-06-12 08:22:02 -0400

    add variable configs

--- server/cmd/web/main.go
+++ server/cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/gob"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -59,12 +60,29 @@ func run() (*driver.DB, error) {
 	gob.Register(models.Restriction{})
 	gob.Register(map[string]int{})
 
+	// Read environment variables
+	inProduction := flag.Bool("production", true, "Run in production mode")
+	useCache := flag.Bool("cache", true, "Use template cache")
+	dbHost := flag.String("dbhost", "localhost", "Database host")
+	dbName := flag.String("dbname", "", "Database name")
+	dbUser := flag.String("dbuser", "", "Database user")
+	dbPass := flag.String("dbpass", "", "Database password")
+	dbPort := flag.String("dbport", "5432", "Database port")
+	dbSSL := flag.String("dbssl", "", "Database ssl settings (disable, prefer, require)")
+
+	flag.Parse()
+
+	if *dbName == "" || *dbUser == "" {
+		fmt.Println("Database name and user must be provided")
+		os.Exit(1)
+	}
+
 	mailChan := make(chan models.MailData)
 
 	app.Mailchan = mailChan
 
-	// Change this value to true in production
-	app.InProduction = false
+	app.InProduction = *inProduction
+	app.UseCache = *useCache
 
 	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	app.InfoLog = infoLog
@@ -81,9 +99,10 @@ func run() (*driver.DB, error) {
 	app.Session = session
 
 	// connnect to database
-
 	log.Println("Connecting to the database")
-	db, err := driver.ConnectSQL("host=localhost port=5432 dbname=bookings user=postgres password=learndb")
+
+	connectionString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s", *dbHost, *dbPort, *dbName, *dbUser, *dbPass, *dbSSL)
+	db, err := driver.ConnectSQL(connectionString)
 	if err != nil {
 		log.Fatal("cannot connect to database", err)
 		return nil, err
@@ -99,7 +118,6 @@ func run() (*driver.DB, error) {
 	}
 
 	app.TemplateCache = templateCache
-	app.UseCache = false
 
 	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
