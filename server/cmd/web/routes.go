2bafdff2f6cf8c494f2e8a2cf908631775dec408
Bijay Tamang <tmg.bijay11@gmail.com>
2025-06-26 09:06:48 -0400

    add go chi cors to allow requests from browser

--- server/cmd/web/routes.go
+++ server/cmd/web/routes.go
@@ -7,11 +7,21 @@ import (
 	"github.com/bijay11/bookings/internal/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 
 	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"}, // allow your frontend origin
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	// Middlewares
 	mux.Use(middleware.Recoverer)
